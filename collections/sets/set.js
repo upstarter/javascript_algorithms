2355a23c9d294d1fe8085a4744d9846a8287e286
WWW.CRYPTOWISE.AI <ericsteen1@gmail.com>
2019-11-12 10:24:41 -0800

    Create set.js

---
+++ collections/sets/set.js
@@ -0,0 +1,8 @@
+// Set Data-Structure
+
+let s = new Set()
+s.add("hello").add("goodbye").add("hello")
+s.size === 2
+s.has("hello") === true
+for (let key of s.values()) // insertion order
+    console.log(key)
