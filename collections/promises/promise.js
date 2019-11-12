363fd3396235644272c0102d94bf93b1fff0c3b2
WWW.CRYPTOWISE.AI <ericsteen1@gmail.com>
2019-11-12 11:08:08 -0800

    Update promise.js

--- collections/promises/promise.js
+++ collections/promises/promise.js
@@ -15,4 +15,26 @@ p.then((res) => {
 });
 
 // still gets 4
-p.then((res) => console.log(res));  
+p.then((res) => console.log(res));
+
+// Promise Combination
+// Combine one or more promises into new promises without having to take care of ordering of the underlying asynchronous operations yourself.
+
+function fetchAsync (url, timeout, onData, onError) {
+    …
+}
+let fetchPromised = (url, timeout) => {
+    return new Promise((resolve, reject) => {
+        fetchAsync(url, timeout, resolve, reject)
+    })
+}
+Promise.all([
+    fetchPromised("http://backend/foo.txt", 500),
+    fetchPromised("http://backend/bar.txt", 500),
+    fetchPromised("http://backend/baz.txt", 500)
+]).then((data) => {
+    let [ foo, bar, baz ] = data
+    console.log(`success: foo=${foo} bar=${bar} baz=${baz}`)
+}, (err) => {
+    console.log(`error: ${err}`)
+})
